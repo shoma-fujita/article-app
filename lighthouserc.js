b7d36a71154b51e02d4ce4b91c6a1fc96b0ea20a
shoma-fujita <syouteni123@gmail.com>
2025-03-27 09:22:35 +0900

    設定変更

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,24 +1,10 @@
-export default {
+module.exports = {
   ci: {
     collect: {
-      staticDistDir: '.output/public',
-    },
-    assert: {
-      assertions: {
-        // パフォーマンススコアが最低でも80以上であることを要求
-        'categories:performance': ['error', { minScore: 0.8 }],
-        // First Contentful Paintは3秒以内
-        'first-contentful-paint': ['error', { maxNumericValue: 3000 }],
-        // Time to Interactiveは5秒以内
-        'interactive': ['error', { maxNumericValue: 5000 }],
-        // Largest Contentful Paintは2.5秒以内
-        'largest-contentful-paint': ['error', { maxNumericValue: 2500 }],
-        // Cumulative Layout Shiftは0.25以下
-        'cumulative-layout-shift': ['error', { maxNumericValue: 0.25 }],
-      },
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'npm run start',
     },
     upload: {
-      // 結果をGitHub上に保存
       target: 'temporary-public-storage',
     },
   },
