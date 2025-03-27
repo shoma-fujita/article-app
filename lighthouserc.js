a72f27b213f5e92e4118ba173ead0a781b8c3f90
shoma-fujita <syouteni123@gmail.com>
2025-03-27 09:27:07 +0900

    変更

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,29 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000/'],
+      // 静的サイトの場合
+      // staticDistDir: './dist',
+      // または実行中のサーバーの場合
       startServerCommand: 'npm run start',
+      url: [
+        'http://localhost:3000/',
+        // 他に測定したいURLを追加
+      ],
+      numberOfRuns: 3, // 各URLに対して実行する回数
     },
     upload: {
-      target: 'temporary-public-storage',
+      target: 'temporary-public-storage', // または独自のサーバー
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        // Web Vitalsに関する特定のアサーション
+        'first-contentful-paint': ['warn', {maxNumericValue: 2000}],
+        'largest-contentful-paint': ['error', {maxNumericValue: 2500}],
+        'cumulative-layout-shift': ['error', {maxNumericValue: 0.1}],
+        'total-blocking-time': ['warn', {maxNumericValue: 300}],
+        'interactive': ['warn', {maxNumericValue: 3500}],
+      },
     },
   },
 };
