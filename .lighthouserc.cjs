d7b106b8fab19fd862ebc46e6d885fc6ea5afbc3
shoma-fujita <syouteni123@gmail.com>
2025-03-29 09:53:39 +0900

    修正

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   ci: {
     collect: {
+      // テスト対象となるURLを再追加
+      url: ['http://localhost:3000/'],
       staticDistDir: '.output/public',
+      // サーバーを自動的に起動するコマンド（プロジェクトに応じて変更）
+      startServerCommand: 'npm run start',
       numberOfRuns: 3,
     },
     upload: {
